08460c62624536ab931f7ec07dca7cee2ae79918
Vansh Khandelwal <125909342+vanshk2003@users.noreply.github.com>
2023-04-25 17:14:34 +0530

    Add files via upload

---
+++ bubble-sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void Bubble(int a[5], int n)
+{
+    int passes,comparisons,i,j;
+    int temp;
+    passes=n-1;
+    comparisons=n-1;
+    
+    for(i=0;i<passes;i++)
+    {
+        for(j=0;j<comparisons-i;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+printf("First Element: %d \nLast Element:%d", a[0],a[4]);
+}
+int main()
+{
+   int arr[5] = {120,30,40,500,100};
+   Bubble(arr,5);
+}
